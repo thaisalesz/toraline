87e5b5f6de386f5467057902f3ed4a19f0c884e2
thaisalesz <thais-sales@live.com>
2023-02-04 19:45:22 -0400

    feat: type aditions to purchase service

--- src/services/purchase.services.ts
+++ src/services/purchase.services.ts
@@ -1,29 +1,29 @@
 import { AppError } from "../errors";
-import { IPurchaseRequestData, Purchase } from "../interfaces";
+import { IProduct, IPurchaseRequestData, IUser, Purchase } from "../interfaces";
 import { listProductsService } from "./products.services";
 import { listUsersService } from "./users.services";
 
 
 const createPurchaseService = async ({userId, productsIds}: IPurchaseRequestData):Promise<Purchase> => {
-    const users = await listUsersService()
-    const products = await listProductsService()
+    const users: IUser[] = await listUsersService()
+    const products: IProduct[] = await listProductsService()
 
-    const user = users.find((user) => user.id == userId)
+    const user: IUser | undefined = users.find((user) => user.id == userId)
     
     if(!user){
         throw new AppError('User not found', 404)
     }
 
-    const userProducts = products.filter((product) => productsIds.includes(product.id))
+    const userProducts:IProduct[] = products.filter((product) => productsIds.includes(product.id))
 
     if(userProducts.length != productsIds.length){
         throw new AppError('Some or all products not found', 404)
     }
 
-    const productsSum = userProducts.reduce((acc, cur) => acc + cur.price, 0)
-    const discount = (productsSum *(Number(user!.tax)/100))/100
+    const productsSum: number = userProducts.reduce((acc, cur) => acc + cur.price, 0)
+    const discount: number = (productsSum *(Number(user!.tax)/100))/100
 
-    const purchaseValue = productsSum - discount
+    const purchaseValue: Purchase = productsSum - discount
 
     return purchaseValue
 }
